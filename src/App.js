89f4a1388a57a20d38362cea1f368d145659110b
Ronnie Geraghty <ronniegerag@gmail.com>
2019-10-05 01:40:32 -0400

    created react components and added them to main app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Nav from "./components/nav";
+import Profile from "./components/profile";
+import Experience from "./components/experience";
+import Projects from "./components/projects";
+import ContactMe from "./components/contactMe";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>APP</h1>
+      <Nav />
+      <Profile />
+      <Experience />
+      <Projects />
+      <ContactMe />
     </div>
   );
 }
